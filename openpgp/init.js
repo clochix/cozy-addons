58df0ed420ea03c2a0e4f81a42da0645a4984b0b
Clochix <clochix@clochix.net>
2015-01-13 17:29:21 +0100

    PGP plugin: validate signature in attachment

--- openpgp/init.js
+++ openpgp/init.js
@@ -3,6 +3,13 @@
 if (typeof window.plugins !== "object") {
   window.plugins = {};
 }
+function render(txt) {
+  var container = document.querySelector('.messageToolbox + .row'),
+      actionbar = document.createElement('div');
+  actionbar.classList.add('content-action');
+  actionbar.textContent = txt;
+  container.insertBefore(actionbar, container.firstChild);
+}
 function checkSignatures(msg) {
   var keys;
   keys = msg.getSigningKeyIds();
@@ -15,9 +22,8 @@ function checkSignatures(msg) {
         if (req.status === 200) {
           openpgp.key.readArmored(req.responseText).keys.forEach(function (pubkey) {
             var users = [];
-            console.log(pubkey);
             pubkey.users.forEach(function (u) {
-              if (typeof u.userId !== 'undefined') {
+              if (typeof u.userId !== 'undefined' && u.userId !== null) {
                 users.push(u.userId.userid);
               }
             });
@@ -29,13 +35,13 @@ function checkSignatures(msg) {
             var pubkeyId = verify.keyid.toHex(),
             name = pubkeyId + " " + pubKeys1[pubkeyId].userNames;
             if (verify.valid === true) {
-              console.log('Good signature by key ' + name);
+              render('Good signature by key ' + name);
             } else {
-              console.log('Wrong signature by key ' + name);
+              render('Wrong signature by key ' + name);
             }
           });
         } else {
-          console.error("Key not found");
+          render("Key not found");
         }
       }
     };
@@ -43,34 +49,56 @@ function checkSignatures(msg) {
   });
 }
 function checkMessage(message) {
-  var cleartext;
+  var cleartext, mailboxId, messageId, boundary, xhr;
   if (/^-----BEGIN PGP SIGNED MESSAGE/.test(message.text)) {
     cleartext = openpgp.cleartext.readArmored(message.text);
     checkSignatures(cleartext);
   } else {
+    mailboxId = window.cozyMails.getCurrentMailbox().id;
+    messageId = message.mailboxIDs[mailboxId];
+    message.headers['content-type'].split(/;\s+/).forEach(function (type) {
+      if (/boundary/.test(type)) {
+        boundary = type.split('=')[1].replace(/"|'/g, '');
+      }
+    });
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', 'raw/' + mailboxId + '/' + messageId, true);
+    xhr.onload = function (e) {
+      var re  = new RegExp("^." + boundary + ".*$", "gim"),
+          raw = xhr.responseText,
+          headers = {},
+          res, parts, packetlist, literalDataPacket, input;
+      raw.split(/^--/gim)[0].replace(/\n\s+/gim, "").split(/\n/gim).forEach(function (h) {
+        var tmp = h.split(':');
+        if (tmp.length === 2) {
+          headers[tmp[0].toLowerCase()] = tmp[1];
+        }
+      });
+      res = /boundary=(.)([^\1]+)\1/.exec(headers['content-type']);
+      if (res && res.length === 3) {
+        boundary = res[2];
+      }
+      re = new RegExp('\r{0,1}\n^-+' + boundary + '-*[\r|\n]+', 'gim');
+      parts = raw.split(re).slice(1);
+      packetlist = new openpgp.packet.List();
+      literalDataPacket = new openpgp.packet.Literal();
+      input = openpgp.armor.decode(parts[1]).data;
+      literalDataPacket.setText(parts[0]);
+      packetlist.push(literalDataPacket);
+      packetlist.read(input);
+      checkSignatures(new openpgp.message.Message(packetlist));
+    };
+    xhr.onerror = function (e) {
+      // @TODO
+    };
+    xhr.send();
+    /*
     message.attachments.forEach(function (attach) {
       if (attach.contentType === 'application/pgp-signature') {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', attach.url, true);
-        xhr.onload = function (e) {
-          console.log(xhr.responseText);
-          var packetlist = new openpgp.packet.List(),
-              literalDataPacket = new openpgp.packet.Literal(),
-              input = openpgp.armor.decode(xhr.responseText).data;
-          console.log(message.text);
-          //@FIXME We need the raw body attachment
-          literalDataPacket.setText(message.text);
-          packetlist.push(literalDataPacket);
-          packetlist.read(input);
-          checkSignatures(new openpgp.message.Message(packetlist));
-        };
-        xhr.onerror = function (e) {
-          // @TODO
-        };
-        xhr.send();
 
       }
     });
+    */
   }
 }
 (function (root) {
@@ -79,8 +107,15 @@ function checkMessage(message) {
     active: true,
     listeners: {
       'MESSAGE_LOADED': function (params) {
-        checkMessage(params.detail);
-      }
+        var message = params.detail;
+        if (/PGP/.test(message.text) ||
+            message.attachments.some(function (a) {
+              return /pgp/.test(a.contentType);
+            })
+           ) {
+              checkMessage(message);
+            }
+        }
     }
   };
-})(window.plugins);
+      })(window.plugins);
