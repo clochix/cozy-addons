98e600ee8e46222d30c24442087707f4a2367601
Clochix <clochix@clochix.net>
2015-03-30 23:45:37 +0200

    Update addon

---
+++ public/addon/data/load.js
@@ -0,0 +1,4 @@
+//jshint browser: true
+var s = document.createElement('script');
+s.src = '/apps/addons/api/addons/lib';
+document.body.appendChild(s);
--- public/addon/index.js
+++ public/addon/index.js
@@ -1,17 +1,22 @@
 /*jshint node: true, browser: true */
 var pageMod = require("sdk/page-mod");
 pageMod.PageMod({
-  include: /.*\.cozycloud.cc\/apps\/calendar.*/,
-  contentScriptFile: ["./calendar.js"],
+  include: /.*\.cozycloud.cc\/apps\/.*/,
+  contentScriptFile: ["./load.js"],
   contentScriptWhen: "end"
 });
-pageMod.PageMod({
-  include: /.*\.cozycloud.cc\/apps\/emails.*/,
-  contentScriptFile: ["./emails.js"],
-  contentScriptWhen: "end"
-});
-pageMod.PageMod({
-  include: /.*\.cozycloud.cc\/apps\/files.*/,
-  contentScriptFile: ["./files.js"],
-  contentScriptWhen: "end"
-});
+//pageMod.PageMod({
+//  include: /.*\.cozycloud.cc\/apps\/calendar.*/,
+//  contentScriptFile: ["./calendar.js"],
+//  contentScriptWhen: "end"
+//});
+//pageMod.PageMod({
+//  include: /.*\.cozycloud.cc\/apps\/emails.*/,
+//  contentScriptFile: ["./emails.js"],
+//  contentScriptWhen: "end"
+//});
+//pageMod.PageMod({
+//  include: /.*\.cozycloud.cc\/apps\/files.*/,
+//  contentScriptFile: ["./files.js"],
+//  contentScriptWhen: "end"
+//});
